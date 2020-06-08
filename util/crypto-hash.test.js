4393295bbece2e5f404e33b8c6a47a49cddebf44
Naina V <nainavaskenly@yahoo.com>
2020-06-08 19:58:09 +0530

    Added Wallet class, Pub/priv keys and transaction object

---
+++ util/crypto-hash.test.js
@@ -0,0 +1,22 @@
+const cryptoHash = require('./crypto-hash');
+
+describe('cryptoHash()', () => {
+    
+    it('generates a SHA-256 hashed output', () => {
+        expect(cryptoHash('foo'))
+        .toEqual('b2213295d564916f89a6a42455567c87c3f480fcd7a1c15e220f17d7169a790b');
+    });
+
+    it('produces same hash with same input arguments in any order', () => {
+        expect(cryptoHash('one', 'two', 'three')).toEqual(cryptoHash('three', 'one', 'two'));
+    });
+
+    // when outputMap is passed for signing, after updating it, the hash remains same as old one. Rectifying that issue
+    it('produces a unique hash when the properties have changed on an input', () => {
+        const foo = {};
+        originalHash = cryptoHash(foo);
+
+        foo['s'] = 'a'
+        expect(cryptoHash(foo)).not.toEqual(originalHash);
+    });
+});
